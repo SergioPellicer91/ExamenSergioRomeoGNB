ef5ab06e4cbcec0af2f6b5e7798bb4227242efbd
SergioPellicer91 <sergio_pelli@hotmail.com>
2019-03-07 20:09:57 +0100

    Contexto y Modelos

---
+++ ExamenSergioRomeoGNB/Models/GnbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GNBSergioRP.Models
+{
+    public class GnbContext : DbContext
+    {
+        public GnbContext(DbContextOptions<GnbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Rate> Rates { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
---
+++ ExamenSergioRomeoGNB/Models/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GNBSergioRP.Models
+{
+    public partial class Transaction
+    {
+        [JsonIgnore]
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
